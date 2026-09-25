3e357babbeafe4b15399d4631eaee424f8375b7c
sakib <mdsakib786ansari@gmail.com>
2026-09-25 12:05:28 +0530

    Prepare backend for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -33,6 +33,7 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`server is started on ${PORT}`);
 });
